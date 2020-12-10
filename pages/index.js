e9b1cd3a8556b9f1c74e657b8ad35b7ab193d00e
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2020-12-10 22:58:44 +0000

    feat: show memes list

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,44 @@
-export default function Index() {
+export async function getEdgeProps() {
+  const response = await fetch('https://raw.githubusercontent.com/louiechristie/louiechristie.github.io/master/memes/items.json');
+
+  const memes = await response.json();
+
+  return {
+    props: {
+      memes,
+    },
+  };
+}
+
+export default function Index(props) {
+  const {memes} = props;
+
   return (
-    <h1>
-      You're running React on the Edge!
-    </h1>
+    <>
+      <h1>Memes</h1>
+
+      <div class="memes-container">
+        <blockquote class="meme-definition" cite="https://en.wiktionary.org/wiki/meme">
+          <p>
+            <strong>Meme</strong> ... (Internet, slang) Something, usually
+            humorous, which is copied and circulated online with slight
+            adaptations, including quizzes, basic pictures, video templates etc.
+          </p>
+          <cite>- Wiktionary, 2020</cite>
+        </blockquote>
+
+        <ul class="memes-list">
+        {memes.map(item => (
+          <a href={`./${item.url}`}>
+            <li key={item.url} class="box-shadow">
+              <img src={item.image} alt={item.alt} />
+              <h2>{item.title}</h2>
+            </li>
+          </a>
+        ))}
+      </ul>
+
+      </div>
+  </>
   );
 }
